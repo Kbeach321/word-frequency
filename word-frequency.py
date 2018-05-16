2cc72a84b25e80507a825553fd10ed5e32cd762e
Kbeach321 <iamkevinbeach@gmail.com>
2018-05-16 19:37:26 -0400

    Listed

--- word-frequency.py
+++ word-frequency.py
@@ -5,8 +5,10 @@ mydict = {}
 
 with open("sample.txt") as infile:
     document = infile.read().replace(".","").replace("?","").replace("!","").lower()
-    for word in document.split():
-        if word not in mydict.keys():
-            mydict[word] = 0
-        mydict[word] += 1
-    print(mydict)
+for word in document.split():
+    if word not in mydict.keys():
+        mydict[word] = 0
+    mydict[word] += 1
+sorted_dict = sorted(mydict.items())
+for word, count in mydict.items():
+    print("{} {}".format(word, count))
